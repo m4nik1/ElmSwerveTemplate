0131ef0552a127a8b7888469005bbadb544197aa
m4nik1 <maniksoomro@hotmail.com>
2025-04-08 13:53:24 -0400

    Added ElmModule

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.commands.Autos;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+  ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+
+  // Driver and operator controls go here
+  private void configureBindings() {
+  }
+
+
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+}
